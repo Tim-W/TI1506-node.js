f321484d2dfea849dcf799cb557e037b9d60da20
Rens Heddes <r.heddes@gmail.com>
2015-12-04 14:38:09 +0100

    Missed client folder

---
+++ client/javascript/client-app.js
@@ -0,0 +1,61 @@
+var main = function () {
+    "use strict";
+
+    //Load items on page load
+    var newTodoForm, updateForm, todoFormText;
+    getItems();
+
+    //On form submit, post the new item
+    newTodoForm = $('#newTodoForm');
+    newTodoForm.submit(function (ev) {
+        $.ajax({
+            type: newTodoForm.attr('method'),
+            url: newTodoForm.attr('action'),
+            data: newTodoForm.serialize(),
+            success: function (data) {
+                getItems();
+            }
+        });
+
+        ev.preventDefault();
+        document.getElementById('todoFormText').value = "";
+    });
+
+    updateForm = $('#updateForm');
+    updateForm.submit(function (ev) {
+        $.ajax({
+            type: updateForm.attr('method'),
+            url: updateForm.attr('action'),
+            data: updateForm.serialize('message'),
+            success: function (data) {
+                getItems();
+            }
+        });
+
+        ev.preventDefault();
+        document.getElementById('todoFormText').value = "";
+    });
+};
+
+function addTodosToList (todos) {
+    var li;
+    var todolist;
+    console.log("Loading todos from server");
+    todolist = document.getElementById("todo-list");
+    todolist.innerHTML = "";
+    for (var key in todos) {
+        li = document.createElement("li");
+        li.innerHTML = "TODO: " + todos[key].message + "<button onclick='updateTodo()'></button>";
+        todolist.appendChild(li);
+    }
+}
+
+function getItems() {
+    $.getJSON("../todos", addTodosToList)
+        .error(function (jqXHR, textStatus, errorThrown) {
+            console.log("error " + textStatus);
+            console.log("incoming Text " + jqXHR.responseText);
+        });
+}
+
+$(document).ready(main);
